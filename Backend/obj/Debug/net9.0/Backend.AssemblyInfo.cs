edefee35afc569b0e6a425c6dd87ffaac8a1c864
32gede <federico.riato0@gmail.com>
2025-04-26 22:30:31 +0200

    Aggiornamento della versione informativa dell'assembly e modifica della cache degli input

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afb7752e819a1f161862cdedd8fe7a112a851931")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0232358676eae53f5af21fab6b6ea1484c13ee6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
