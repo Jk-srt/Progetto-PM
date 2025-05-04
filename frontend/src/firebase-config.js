861c6547b24bb9b078eacd1039107be79b5d46b1
32gede <federico.riato0@gmail.com>
2025-05-04 18:34:43 +0200

    fix: update storageBucket URL in firebase configuration for correct deployment

--- frontend/src/firebase-config.js
+++ frontend/src/firebase-config.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyA4kTnlAVRxHmr5MdRH0MWrknyT-z3w7ag",
   authDomain: "finance-management-7c778.firebaseapp.com",
   projectId: "finance-management-7c778",
-  storageBucket: "finance-management-7c778.appspot.com", // Corretto
+  storageBucket: "finance-management-7c778.web.app", // Corretto
   messagingSenderId: "41019168831",
   appId: "1:41019168831:web:afb09410b88bfa8b09408a",
   measurementId: "G-537WX6M5CG"
